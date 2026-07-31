db1a547b772b6511ec9bb028a011dc3570ed4269
Pushti Sharma <13pushtiii@gmail.com>
2026-07-31 16:02:13 +0530

    Configure app for Render

--- app.py
+++ app.py
@@ -41,5 +41,8 @@ def predict():
             "prediction": "Normal Transaction"
         })
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
